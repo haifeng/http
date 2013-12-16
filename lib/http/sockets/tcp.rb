21944e3bdc6fc8a58aced950d86387e2792ed2d8
Haifeng Cao <caohaifeng@gmail.com>
2013-12-16 15:30:16 -0500

    use Hash to cache socket

--- lib/http/sockets/tcp.rb
+++ lib/http/sockets/tcp.rb
@@ -3,20 +3,22 @@ require 'http/sockets/base'
 require 'pry'
 
 module Sockets
-
+  SOCKET_HASH_KEY="HTTP::SOCKET_HASH"
   class TCP < Socket
     include Base
+
+    Thread.current[SOCKET_HASH_KEY] ||= {}
 
     # should check if is keep_alive
     def self.open(host, port, timeout=10)
       key = "#{host}::#{port}::#{timeout}"
 
-      socket = Thread.current[key]
+      socket = Thread.current[SOCKET_HASH_KEY][key]
       if socket && !socket.closed?
         socket
       else
         socket = create_socket(host, port, timeout)
-        Thread.current[key] = socket if port != 443
+        Thread.current[SOCKET_HASH_KEY][key] = socket if port != 443
         socket
       end
     end
